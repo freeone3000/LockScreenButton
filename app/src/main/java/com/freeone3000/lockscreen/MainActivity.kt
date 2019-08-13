41738545dd44912d572cab00261665e8eaa7020c
James Moore <jamoo@microsoft.com>
2019-08-13 17:16:28 -0400

    Attempts to power off phone using lock screen delay, with hard lock as fallback.

--- app/src/main/java/com/freeone3000/lockscreen/MainActivity.kt
+++ app/src/main/java/com/freeone3000/lockscreen/MainActivity.kt
@@ -4,13 +4,19 @@ import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.hardware.display.DisplayManager
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
 import android.widget.Toast
+import java.util.*
 
 const val ADMIN_INTENT = 2333
 
 class MainActivity : AppCompatActivity() {
+    var compName: ComponentName? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerDeviceAdministrator()
@@ -23,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         val description = "Enable this if you're James Moore or you're dumb."
 
         val mdp = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
-        val compName = ComponentName(this, ScreenLocker::class.java)
+        compName = ComponentName(this, ScreenLocker::class.java)
 
-        if(mdp.isAdminActive(compName)) {
+        if(mdp.isAdminActive(compName!!)) {
             activateDeviceAdminAndLockScreen()
         } else {
             val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
@@ -39,8 +45,24 @@ class MainActivity : AppCompatActivity() {
      * Actually become the device administrator, after the callback
      */
     private fun activateDeviceAdminAndLockScreen() {
+        val delay = 200L //in *ms*
+
         val mdp = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
-        mdp.lockNow()
+        val oldDelay = mdp.getMaximumTimeToLock(compName)
+        mdp.setMaximumTimeToLock(compName!!, delay)
+
+        Thread.sleep((delay * 3) / 2)
+        mdp.setMaximumTimeToLock(compName!!, oldDelay)
+
+        // if screen still on, use the backup
+        val dm = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        dm.displays.forEach {
+            if(it.state == Display.STATE_ON) {
+                mdp.lockNow()
+                return@forEach
+            }
+        }
+
         finish()
     }
 
